56481e1de75895cb9bc443fca818ff1c0aba0033
Jonathan Johnson <jonathan.johnson@dijure.com>
2019-10-09 22:27:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Java Project for Demonstation
+
+This project contains some simple, generic Java code in a Gradle project used to demonstrate building Java into a container.
+
+See the Katacoda scenario for [Distillation Pattern with the JRE](https://www.katacoda.com/javajon/courses/kubernetes-fundamentals/distillation) to learn how it's used.
+
+The code was inspired from the related article [GraalVM: Native Images in Containers](https://blogs.oracle.com/javamagazine/graalvm-native-images-in-containers) by [Oleg Šelajev](https://github.com/shelajev).
---
+++ src/main/java/ListDir.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ListDir {
+    public static void main(String[] args)throws java.io.IOException {
+        String root = ".";
+        if (args.length > 0) {
+            root = args[0];
+        }
+        System.out.println("Walking path: " + Paths.get(root));
+        long size[] = { 0 };
+        long count[] = { 0 } ;
+        try (Stream<Path> paths = Files.walk(Paths.get(root))) {
+            paths.filter(Files::isRegularFile).forEach((Path p) -> {
+                File f = p.toFile();
+                size[0] += f.length();
+                count[0]++;
+                System.out.println(f.getPath());
+            });
+        }
+        System.out.println(count[0] + " files with " + size[0] + " bytes");
+    }
+}
